f53bc5857b75ba8f91a48ba6824161ae0d049ae7
Alex Luong <alex@alexluong.com>
2021-12-23 03:49:25 -0800

    chore: setup tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <p>Hello, World!</p>
+    <div className="flex justify-center items-center h-screen">
+      <p className="text-green-500 text-5xl">Hello, World!</p>
     </div>
   );
 };
